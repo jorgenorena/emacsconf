e808fb3323484f76d70b7880e68d84f1dd8327d7
Jorge Noreña <jonorena@yahoo.com>
2024-05-17 06:49:26 -0400

    add theme, font, cursor change, ivy search

--- init.el
+++ init.el
@@ -2,13 +2,9 @@
 ;;
 ;; Copyright (C) 2024 Jorge
 ;;
-;; Author: Jorge <jorge@jorge-XENIA-15>
-;; Maintainer: Jorge <jorge@jorge-XENIA-15>
-;; Created: mayo 16, 2024
-;; Modified: mayo 16, 2024
 ;; Version: 0.0.1
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/jorge/init
+;; Homepage: https://github.com/jnorena/emacsconf
 ;; Package-Requires: ((emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
@@ -19,15 +15,29 @@
 ;;
 ;;; Code:
 
+;; --- Remove UI elements ---
+
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (menu-bar-mode -1)
 (set-fringe-mode 10)
 
+;; --- Theme ---
+
+(load-theme 'tango-dark)
+
+;; --- Font ---
+
+(set-face-attribute 'default nil :height 140)
+
+;; --- Package manager ---
+
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '(("melpa" . "https://melpa.org/packages/")
+	     ("org", "https://orgmode.org/elpa/")
+	     ("elpa", "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -35,11 +45,11 @@
   (package-install 'use-package))
 
 (require 'use-package)
+(setq use-package-always-ensure t)
 
-;; ---------------- EVIL MODE -----------------------
+;; --- EVIL MODE ---
 
 (use-package evil
-  :ensure t
   :init
   (setq evil-want-integration t) ; This is optional, required for some packages
   (setq evil-want-keybinding nil)
@@ -50,13 +60,11 @@
 
 (use-package evil-collection
   :after evil
-  :ensure t
   :config
   (evil-collection-init))
 
 (use-package evil-surround
   :after evil
-  :ensure t
   :config
   (global-evil-surround-mode 1))
 
@@ -69,22 +77,42 @@
 
 (use-package evil-commentary
   :after evil
-  :ensure t
   :config
   (evil-commentary-mode))
 
 (use-package evil-multiedit
   :after evil
-  :ensure t
   :config
   (evil-multiedit-default-keybinds))
 
 (use-package evil-snipe
   :after evil
-  :ensure t
   :init
   (evil-snipe-mode 1))  ; Globally enable evil-snipe
 
+(use-package evil-terminal-cursor-changer
+  :config
+  (evil-terminal-cursor-changer-activate) ; or (etcc-on)
+  )
+
+;; --- Ivy search ---
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
 
 (provide 'init)
 ;;; init.el ends here
@@ -94,7 +122,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-snipe evil-commentary evil-leader evil-surround evil-collection evil use-package)))
+   '(ivy evil-terminal-cursor-changer evil-snipe evil-commentary evil-leader evil-surround evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
